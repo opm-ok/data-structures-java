5d6c329eed2ef75d6eac2d49e047706765fe2ff8
Ranbir More <rmore@questrade.com>
2020-01-13 16:02:32 -0500

    Added doubly linked list

---
+++ doublyLinkedList/src/DoublyLinkedList.java
@@ -0,0 +1,13 @@
+public class DoublyLinkedList {
+
+    private Node head;
+    private Node next;
+    private Node previous;
+
+
+    // Insert data at the beginning of the list
+    private void add(int data){
+
+    }
+
+}
---
+++ doublyLinkedList/src/Node.java
@@ -0,0 +1,32 @@
+public class Node {
+
+    private int data;
+    private Node previous;
+    private Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+
+
