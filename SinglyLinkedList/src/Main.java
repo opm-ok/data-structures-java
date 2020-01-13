507633282443405721383d9bb63b0cdc9c29f3a9
Ranbir More <rmore@questrade.com>
2020-01-13 13:11:52 -0500

    Updated singly linked list

--- SinglyLinkedList/src/Main.java
+++ SinglyLinkedList/src/Main.java
@@ -8,9 +8,14 @@ public class Main {
         singlyLinkedList.add(44);
         singlyLinkedList.add(99);
         singlyLinkedList.add(55);
+        singlyLinkedList.add(88);
+        singlyLinkedList.add(109);
+        singlyLinkedList.add(250);
+        singlyLinkedList.add(500);
+        singlyLinkedList.add(950);
         singlyLinkedList.remove(1);
 
         singlyLinkedList.printList();
-
     }
 }
+
