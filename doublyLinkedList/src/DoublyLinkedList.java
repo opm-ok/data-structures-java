f7cb536e65d20c0b2e2ac198d56072a65ae2b43b
opm-ok <ranbirmore@gmail.com>
2020-01-13 23:56:36 -0500

    updated doubly linked list

--- doublyLinkedList/src/DoublyLinkedList.java
+++ doublyLinkedList/src/DoublyLinkedList.java
@@ -1,13 +1,61 @@
 public class DoublyLinkedList {
 
     private Node head;
+    private Node tail;
     private Node next;
     private Node previous;
+    private int size;
 
+    public int getSize() {
+        return this.size;
+    }
 
-    // Insert data at the beginning of the list
-    private void add(int data){
+    // Insert data at the front of the list
+    public void addToHead(int data){
+        Node newNode = new Node(data);
+        newNode.setNext(head);
+
+        if (head == null){
+            head = newNode;
+        } else {
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+    }
+
+    // Insert data at the end of the list
+    public void addToTail(int data){
+        Node newNode = new Node(data);
+
+        // Insert in an empty list
+        if (tail == null){
+            head = newNode;
+            this.size++;
+
+        } else {
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail = newNode;
+            this.size++;
+            }
 
     }
 
+
+    public void printList(){
+        Node currentNode = head;
+
+        if (currentNode != null){
+            System.out.print("Head -> ");
+            while (currentNode.getNext() != null){
+                System.out.print(currentNode.getData());
+                System.out.print(" <=> ");
+                currentNode = currentNode.getNext();
+            }
+            System.out.print(currentNode.getData() + " -> Null");
+        } else {
+            System.out.println("Empty List");
+        }
+    }
 }
--- doublyLinkedList/src/Main.java
+++ doublyLinkedList/src/Main.java
@@ -2,6 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+
+        linkedList.addToHead(10);
+        linkedList.addToHead(8);
+        linkedList.addToHead(12);
+        linkedList.addToHead(7);
+        linkedList.addToHead(-10);
+        linkedList.addToHead(55);
+
+
+        linkedList.printList();
+
 
     }
 }
