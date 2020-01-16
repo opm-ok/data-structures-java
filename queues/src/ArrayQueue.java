10b184af40c7511e11ad827b3b1a60b53a51239a
Ranbir More <rmore@questrade.com>
2020-01-16 14:25:15 -0500

    array queue completed

--- queues/src/ArrayQueue.java
+++ queues/src/ArrayQueue.java
@@ -6,12 +6,53 @@ public class ArrayQueue {
 
     public ArrayQueue(int capacity){
         this.queue = new SuperHero[capacity];
-        this.front = 0;
-        this.back = 0;
     }
 
     public void add(SuperHero superHero){
 
+        // Queue is full, need to increase capacity
+        if (back == queue.length){
+            SuperHero[] newArray = new SuperHero[queue.length * 2];
+            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            queue = newArray;
+        }
+        // add to queue and increment back
+        queue[back++] = superHero;
+    }
 
+    public SuperHero remove(){
+
+        // return null if the list is empty
+        if (size() == 0){
+            return null;
+        }
+
+        // remove the front item from the queue
+        SuperHero removedSuperHero = queue[front];
+        queue[front++] = null;
+
+        // reset front and back when size = 0
+        if (size() == 0){
+            front = 0;
+            back = 0;
+        }
+        return removedSuperHero;
+    }
+
+    public SuperHero peek(){
+        if (size() == 0){
+            return null;
+        }
+        return queue[front];
+    }
+
+    public int size(){
+        return back - front;
+    }
+
+    public void print(){
+        for (int i = front; i < back; i++) {
+            System.out.println("\t" + queue[i]);
+        }
     }
 }
--- queues/src/Main.java
+++ queues/src/Main.java
@@ -2,7 +2,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        ArrayQueue queue = new ArrayQueue(5);
 
+        SuperHero batman = new SuperHero("Bruce", "Wayne", 1000);
+        SuperHero superman = new SuperHero("Clark", "Kent", 9000);
+        SuperHero spiderman = new SuperHero("Peter", "Parker", 1250);
+        SuperHero hulk = new SuperHero("Bruce", "Banner", 4500);
+        SuperHero ironman = new SuperHero("Tony", "Stark", 2500);
+        SuperHero shazam = new SuperHero("Billy", "Batson", 3000);
+
+        queue.add(batman);
+        queue.add(superman);
+        queue.add(spiderman);
+        queue.add(hulk);
+        queue.add(ironman);
+        queue.add(shazam);
+
+        queue.remove();
+        System.out.println("Size of Queue: " + queue.size());
+        System.out.println("Peek: " + queue.peek());
+
+        System.out.println("\nQueue printed front to back: ");
+        queue.print();
     }
 }
 
